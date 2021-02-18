59ebe2a2af4229cda0ace8cad4ab9edf8a3ecb3c
cuiwufeng <ttzxc@outlook.com>
2021-02-18 23:15:21 +0800

    修改

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -2,15 +2,36 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.MailClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+import java.util.Map;
 
 @Service
 public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextpath;
+
     public User findUserById(int id){
         return userMapper.selectById(id);
+    }
+
+    public Map<String,Object> register(User user){
+        return null;
     }
 }
