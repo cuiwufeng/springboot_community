79eb6dc19b57d8da7086b58d07d17aad7e3b6b36
cuiwufeng <ttzxc@outlook.com>
2021-03-03 23:00:32 +0800

    登录功能及退出登录

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -16,4 +16,14 @@ public interface CommunityConstant {
      * 激活失败
      */
     int ACTIVATION_FAILURE = 2;
+
+    /**
+     * 默认状态登录凭证的超时时间
+     */
+    int DEFAULT_EXPIRED_SECONDS = 12 * 3600;
+
+    /**
+     * 记住状态登录凭证的超时时间
+     */
+    int REMEMBER_EXPIRED_SECONDS = 24 * 3600 * 100;
 }
